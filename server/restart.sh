b321eec09f8041e9c1d2103123dded33912d324d
Dominik Meyer <domeyer@TB-L169.corp.ad.zalando.net>
2025-01-21 08:09:52 +0100

    change workflow again

--- server/restart.sh
+++ server/restart.sh
@@ -1,8 +1,5 @@
 # kill all docker
 docker kill $(docker ps -q)
 
-# install npm
-docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app node npm install
-
 # start our docker
-docker run --rm -p9000:9000 -v "$PWD":/usr/src/app -w /usr/src/app node server.js
+docker run -d --rm -p9000:9000 -v "$PWD":/usr/src/app -w /usr/src/app node server.js
