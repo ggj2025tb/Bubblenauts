f8f24a7a2d4af72f36579991294b2a164e2e8866
Mika Stiebitz <mika.stiebitz@tradebyte.com>
2025-01-25 13:07:18 +0100

    feat: Change player movement speed

--- client/src/classes/Player.ts
+++ client/src/classes/Player.ts
@@ -1,12 +1,15 @@
 import { Actor } from './Actor'
-import type { Socket } from 'socket.io-client'
+import { Socket } from 'socket.io-client'
 
 export class Player extends Actor {
+    private readonly MOVEMENT_SPEED = 180
+    private readonly MAX_SPEED = 160
+    private readonly DRAG = 800
+
     private keyW: Phaser.Input.Keyboard.Key
     private keyA: Phaser.Input.Keyboard.Key
     private keyS: Phaser.Input.Keyboard.Key
     private keyD: Phaser.Input.Keyboard.Key
-
     private socket: Socket
 
     constructor(scene: Phaser.Scene, x: number, y: number, socket: Socket) {
@@ -20,26 +23,28 @@ export class Player extends Actor {
         // PHYSICS
         this.getBody().setSize(30, 30)
         this.getBody().setOffset(8, 0)
+        this.getBody().setDrag(this.DRAG, this.DRAG)
+        this.getBody().setMaxVelocity(this.MAX_SPEED, this.MAX_SPEED)
     }
+
     update(): void {
-        this.getBody().setVelocity(0)
         if (this.keyW?.isDown) {
-            this.body.velocity.y = -110
+            this.body.velocity.y = -this.MOVEMENT_SPEED
         }
         if (this.keyA?.isDown) {
-            this.body.velocity.x = -110
+            this.body.velocity.x = -this.MOVEMENT_SPEED
             this.checkFlip()
             this.getBody().setOffset(48, 15)
         }
         if (this.keyS?.isDown) {
-            this.body.velocity.y = 110
+            this.body.velocity.y = this.MOVEMENT_SPEED
         }
         if (this.keyD?.isDown) {
-            this.body.velocity.x = 110
+            this.body.velocity.x = this.MOVEMENT_SPEED
             this.checkFlip()
             this.getBody().setOffset(15, 15)
         }
-        // Weißßßßß ich nicht digga
+
         if (this.body.velocity.x !== 0 || this.body.velocity.y !== 0) {
             this.socket.emit('playerUpdate', {
                 x: this.x,
