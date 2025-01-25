a1c23209d17faa2aa8e2a13a53544b685f99461a
Dennis Riemer <dennis.riemer@tradebyte.com>
2025-01-25 16:38:06 +0100

    update intro page

--- client/src/scenes/Menu.ts
+++ client/src/scenes/Menu.ts
@@ -20,9 +20,15 @@ export default class Menu extends Phaser.Scene {
 
         this.socket = this.registry.get('socket')
         this.nameInput = document.createElement('input')
-        this.nameInput.style.width = '200px'
-        this.nameInput.style.height = '40px'
+        this.nameInput.style.width = '800px'
+        this.nameInput.style.height = '120px'
         this.nameInput.style.textAlign = 'center'
+        this.nameInput.style.backgroundColor = 'rgba(0, 0, 0, 0.0)'
+        this.nameInput.style.border = 'none'
+        this.nameInput.style.outline = 'none'
+        this.nameInput.style.fontSize = '50px'
+        this.nameInput.maxLength = 20
+
         this.nameInput.placeholder = 'Enter your name'
         this.nameInput.onkeyup = (e) => {
             if (e.key == 'Enter') {
@@ -33,16 +39,19 @@ export default class Menu extends Phaser.Scene {
         this.div.style.width = '100%'
         this.div.style.textAlign = 'center'
         this.div.style.position = 'absolute'
-        this.div.style.top = '250px'
+        this.div.style.top = '470px'
         this.div.appendChild(this.nameInput)
 
         this.div.appendChild(document.createElement('br'))
         this.div.appendChild(document.createElement('br'))
+        this.div.appendChild(document.createElement('br'))
+        this.div.appendChild(document.createElement('br'))
 
         this.button = document.createElement('button')
-        this.button.style.height = '40px'
-        this.button.style.width = '200px'
-        this.button.textContent = 'Join Game'
+        this.button.style.height = '100px'
+        this.button.style.width = '450px'
+        this.button.style.opacity = '0.0'
+        // this.button.textContent = 'Join Game'
         this.button.onclick = () => this.joinGame(this)
         this.div.appendChild(this.button)
 
